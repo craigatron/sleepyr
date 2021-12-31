48070a24e15be7918891a6e469fa2a6770d8807a
Craig Martek <craigmartek@gmail.com>
2021-12-30 23:42:49 -0500

    fix imports, import initial player map

--- sleepyr/client.py
+++ sleepyr/client.py
@@ -1,8 +1,9 @@
+import logging
 from typing import Optional
 
 import requests
 
-import exceptions
+from . import exceptions
 
 SLEEPER_BASE_API_URL = 'https://api.sleeper.app/v1'
 SLEEPER_BASE_API_LEAGUE_URL = f'{SLEEPER_BASE_API_URL}/league'
@@ -10,7 +11,15 @@ SLEEPER_BASE_API_LEAGUE_URL = f'{SLEEPER_BASE_API_URL}/league'
 
 class SleeperClient(object):
 
-    def _request(self, path: str, league_id: Optional[str]):
+    def __init__(self, init_players = True):
+        # TODO: check for staleness
+        if init_players:
+            self._players = self._request('players/nfl')
+        else:
+            logging.warn('Not initializing players map, things will probably break!')
+            self._players = {}
+
+    def _request(self, path: str, league_id: Optional[str] = None):
         full_path = f'{SLEEPER_BASE_API_LEAGUE_URL}/{league_id}/{path}' if league_id else f'{SLEEPER_BASE_API_URL}/{path}'
 
         r = requests.get(full_path)
